8e0a562bdb86e81915cc4bdfc2db28cc0fb8cfff
SnO2WMaN <sno2wman.official@gmail.com>
2020-09-24 16:48:26 +0900

    feat: :sparkles: ユーティリティ関数を作成

---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+export function isEmptyLine(line: string): boolean {
+  return !line.trim();
+}
+
+export function cutEmptyLines(lines: string[]): string[] {
+  return lines.filter((line) => !isEmptyLine(line));
+}
+
+export function trimLastEmptyLines(lines: string[]): string[] {
+  let i = lines.length - 1;
+  while (isEmptyLine(lines[i]) && isEmptyLine(lines[i - 1])) i -= 1;
+  return lines.slice(0, i + 1);
+}
